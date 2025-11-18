bb3bdd5489a6de13fa2bf848003850c9cd9c5525
Phillip Gibson <phillipgibson@fastmail.com>
2025-11-18 16:45:56 -0500

    skills reduce to 3 cols on small screens

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -4,7 +4,7 @@ import skillsData from '../data/skills'
 export default () => (
   <section id="skills" className="py-20 shadow-lg bg-gray-200">
     <h1 className="text-4xl font-bold text-center">Skills</h1>
-    <div className="mt-8 max-w-2xl mx-auto grid grid-cols-4 gap-8">
+    <div className="mt-8 max-w-2xl mx-auto grid grid-cols-3 md:grid-cols-4 gap-8">
       {skillsData.map((skill, i) => (
         <SkillCircle key={i} {...skill} />
       ))}
